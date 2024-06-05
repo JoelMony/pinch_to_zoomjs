372d85c2d8c6c2a5d0541862df1632302025f4a2
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2024-06-05 15:25:35 +0200

    Implement live dev server.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,10 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+    port: 3000,
+  },
 });
